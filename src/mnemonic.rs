a87b4f0c8ec18d6f412ab3584cc7d4ae45e69e4f
赵宏健 <757566833@qq.com>
2023-10-27 18:45:55 +0800

    test

--- src/mnemonic.rs
+++ src/mnemonic.rs
@@ -3,11 +3,13 @@ use k256::{elliptic_curve::sec1::ToEncodedPoint, AffinePoint, SecretKey};
 use sha2::Sha512;
 use sha3::{Digest, Keccak256};
 use std::str::FromStr;
+#[derive(Debug)]
 pub struct HDNode {
     entryop: Vec<u8>,
     seed: [u8; 64],
     address: Vec<u8>,
     private_key: [u8; 32],
+    un_compressed_public_key: Vec<u8>,
     public_key: Vec<u8>,
     chain_code: [u8; 32],
     path: String,
@@ -85,6 +87,7 @@ pub fn get_master_by_mnemonic_str(mnemonic_str: &str) -> Result<HDNode, CustomEr
         address,
         private_key,
         public_key: compress_public_key,
+        un_compressed_public_key: un_comporess_affine_point.to_vec(),
         chain_code,
         path: "m".to_string(),
         index: 0,
@@ -92,18 +95,83 @@ pub fn get_master_by_mnemonic_str(mnemonic_str: &str) -> Result<HDNode, CustomEr
     });
 }
 
-// pub fn get_children_node_by_path(node:HDNode,path:String)-> Result<HDNode, CustomError>{
-//     let components = path.split("/");
-//     for component in components {
-//         if component.contains("'"){
-//             let index = component.replace("'", "").parse::<u64>()?+HARDENED_BIT;
-//             // index, this.chainCode, this.publicKey, this.privateKey
-//         }else{
-//             let index = component.replace("'", "").parse::<u64>()?;
-//         }
-//     }
-//     return  Ok(HDNode { entryop: (), seed: (), address: (), private_key: (), public_key: (), chain_code: (), path: (), index: (), depth: () });
-// }
+pub fn get_children_node_by_path(node: HDNode, path: String) -> Result<HDNode, CustomError> {
+    let mut target: HDNode = node;
+    let components = path.split("/");
+    for component in components {
+        if component.contains("m") {
+            continue;
+        }
+        let index;
+        if component.contains("'") {
+            println!("{}", component.replace("'", ""));
+            index = component.replace("'", "").parse::<u64>()? + HARDENED_BIT;
+            // index, this.chainCode, this.publicKey, this.privateKey
+        } else {
+            println!("{}", component);
+            index = component.parse::<u64>()?;
+        }
+        println!("index is {}", index);
+        let (il, ir) = ser_i(
+            index,
+            &target.chain_code,
+            &target.public_key,
+            &target.private_key,
+        )?;
+        let n = num_bigint::BigUint::parse_bytes(
+            "fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141".as_bytes(),
+            16,
+        )
+        .unwrap();
+        println!("parse n:{}", n.to_string());
+        // todo
+        let code_bytes = (vec_u8_to_biguint(il)
+            + vec_u8_to_biguint(target.private_key.to_vec()) % n)
+            .to_bytes_be();
+        println!("{}",hex::encode(code_bytes.clone()));
+        let mut private_key = [0; 32];
+        private_key.copy_from_slice(&code_bytes[0..32]);
+        let mut chain_code = [0; 32];
+        chain_code.copy_from_slice(&code_bytes[32..]);
+        let secret_key = SecretKey::from_slice(&private_key)?;
+        // 压缩公钥匙 hex
+        let compress_public_key = secret_key.public_key().to_sec1_bytes().to_vec();
+
+        let affine_point = AffinePoint::from(secret_key.public_key());
+        // 非压缩公钥
+        let un_comporess_affine_point = affine_point.to_encoded_point(false).to_bytes();
+        // println!("{}", hex::encode(un_comporess_affine_point));
+
+        let mut hasher = Keccak256::new();
+        // 去掉开头的 02、03、04
+        hasher.update(&un_comporess_affine_point[1..]);
+        let address_vec: Vec<u8> = hasher.finalize().to_vec();
+
+        let address = address_vec[12..].to_vec();
+
+        target.address = address;
+        target.private_key = private_key;
+
+        target.un_compressed_public_key = un_comporess_affine_point.to_vec();
+        target.public_key = compress_public_key;
+        let mut chain_code = [0; 32];
+        chain_code.copy_from_slice(&ir[0..32]);
+        target.chain_code = chain_code;
+        // target = HDNode {
+        //     entryop: a,
+        //     seed: node.seed.clone(),
+        //     address: todo!(),
+        //     private_key: todo!(),
+        //     un_compressed_public_key: todo!(),
+        //     public_key: todo!(),
+        //     chain_code: todo!(),
+        //     path,
+        //     index,
+        //     depth: todo!(),
+        // }
+    }
+    return Ok(target);
+}
 pub fn ser_i(
     index: u64,
     chain_code: &[u8; 32],
@@ -136,7 +204,7 @@ pub fn vec_u8_to_biguint(vec: Vec<u8>) -> num_bigint::BigUint {
 #[cfg(test)]
 mod tests {
 
-    use crate::mnemonic::get_master_by_mnemonic_str;
+    use crate::mnemonic::{get_children_node_by_path, get_master_by_mnemonic_str};
 
     use super::{ser_i, vec_u8_to_biguint};
 
@@ -164,12 +232,17 @@ mod tests {
             hex::encode(master_node.public_key)
         );
         assert_eq!(
+            "0453f006bb6bcf336fef39d827a245d7bef5555ce0cf0d27363b6cc080f1467b935bc89bdb5c09a4d07437f210a2177c4f263819344ee881bf6578566ec2e6bfcd",
+            hex::encode(master_node.un_compressed_public_key)
+        );
+
+        assert_eq!(
             "2f42b653f585b3bc8448187dce37eebaf8da2812",
             hex::encode(master_node.address)
         );
         // m2.to_seed("")
     }
-    
+
     #[test]
     fn test_ser_i() {
         let index: u64 = 2147483692;
@@ -209,8 +282,20 @@ mod tests {
     fn test_vec_to_big_num() {
         let res = vec_u8_to_biguint(vec![
             243, 247, 234, 180, 36, 1, 115, 65, 160, 119, 90, 123, 55, 214, 193, 239, 218, 180,
-            105, 245, 243, 200, 152, 191, 70, 91, 134, 126, 218, 138, 71, 216
+            105, 245, 243, 200, 152, 191, 70, 91, 134, 126, 218, 138, 71, 216,
         ]);
-        assert_eq!("110350053295961381469194980161768063875012486900999354933219462641092364224472",res.to_string())
+        assert_eq!(
+            "110350053295961381469194980161768063875012486900999354933219462641092364224472",
+            res.to_string()
+        )
+    }
+    #[test]
+    fn test_children_node() {
+        let master_node = get_master_by_mnemonic_str(
+            "soldier shell mango cricket future true olympic sleep cupboard easy record hero",
+        )
+        .unwrap();
+        let res = get_children_node_by_path(master_node, "m/44'/60'/0'/0/0".to_string()).unwrap();
+        println!("{:?}", res);
     }
 }
